958376f7977f5e84015b19dfa38080ebf105f5e9
Vishnu Mohan <vishnu@graspio.com>
2018-12-12 17:35:26 +0530

    Finished BFS Graph with comments.

--- bfs-graph.py
+++ bfs-graph.py
@@ -1,30 +1,45 @@
+# Time Complexity: O(V+E) where V is number of vertices in the graph and E is number of edges in the graph.
+
 from collections import defaultdict
 
 
 class Graph:
+
+    # Constructor
     def __init__(self):
+        # default dictionary of list as values to store graph
         self.graph = defaultdict(list)
 
+    # function to add an edge to graph
     def addEdge(self, u, v):
         self.graph[u].append(v)
 
+    # function to add an edge to graph
     def BFS(self, s):
+
+        # Mark all the vertices as not visited
         visited = [False]*(len(self.graph))
-        print('graph', self.graph, end='\n')
+
+        # Create a queue for BFS
         queue = []
+
+        #  Mark the source node as visited and enqueue it
         queue.append(s)
         visited[s] = True
+
+        # Get all adjacent vertices of the dequeued vertex s. If a adjacent
+        # has not been visited, then mark it visited and enqueue it
         while queue:
             s = queue.pop(0)
-            # print(s, end=' ')
+            print(s, end=' ')
             for i in self.graph[s]:
-                print(i, end=' ')
                 if visited[i] == False:
                     queue.append(i)
                     visited[i] = True
-        print(' ')
+        print(end='\n')
 
 
+# Create a graph
 g = Graph()
 g.addEdge(0, 1)
 g.addEdge(0, 2)
@@ -33,5 +48,6 @@ g.addEdge(2, 0)
 g.addEdge(2, 3)
 g.addEdge(3, 3)
 
-print("Following is Breadth First Traversal (starting from vertex 2)")
-g.BFS(2)
+print("Following is Breadth First Traversal (starting from vertex 0)")
+
+g.BFS(0)
